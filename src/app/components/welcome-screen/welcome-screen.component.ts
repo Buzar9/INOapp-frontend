7deb9157eb565f7cc7058c35463ce8d5e149b14c
Marek <marek.buzarewicz@gmail.com>
2025-11-30 10:15:09 +0100

    2.29 Added parameter to hide organizer button.

--- src/app/components/welcome-screen/welcome-screen.component.ts
+++ src/app/components/welcome-screen/welcome-screen.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-welcome-screen',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, CommonModule],
   templateUrl: './welcome-screen.component.html',
   styleUrls: ['./welcome-screen.component.css']
 })
 export class WelcomeScreenComponent {
-    constructor(private router: Router) {}
+  @Input() showOrganizerOption: boolean = false;
+
+  constructor(private router: Router) {}
 
   navigateToOrganizer(): void {
     this.router.navigate(['/organizer']);
