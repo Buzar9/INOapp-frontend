a656270829d4a436ec85201d517dec7fe0652785
Marek <marek.buzarewicz@gmail.com>
2025-12-04 14:04:24 +0100

    3.37 Add statistics after finish.

--- src/app/services/response/RunMetricAfterControlPoint.ts
+++ src/app/services/response/RunMetricAfterControlPoint.ts
@@ -1,7 +1,10 @@
+import { ControlPointView } from './ControlPointView';
+
 export type RunMetricAfterControlPoint = {
     startTime: number,
     finishTime: number,
     mainTime: number,
+    controlPoints: ControlPointView[],
     checkpointsNumber: number,
     wasActivate: boolean,
     isFinished: boolean,
